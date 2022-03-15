fa626731df0015ae83623aff227e174e55d1127b
xchen586 <xchen586@gmail.com>
2022-03-15 12:09:07 -0400

    Add files via upload
    Initial draft.

---
+++ src/vb_sdk/irelease.h
@@ -0,0 +1,14 @@
+#ifndef MANYCAM_VB_INCLUDE_IRELEASE_H
+#define MANYCAM_VB_INCLUDE_IRELEASE_H
+
+namespace manycam {
+
+class IRelease
+{
+public:
+	virtual void release() = 0;
+};
+
+}
+
+#endif
---
+++ src/vb_sdk/pipeline.h
@@ -0,0 +1,42 @@
+#ifndef MANYCAM_VB_INCLUDE_PIPELINE_H
+#define MANYCAM_VB_INCLUDE_PIPELINE_H
+
+#include "frame.h"
+
+namespace manycam {
+
+enum PipelineErrorCode
+{
+	ok = 0,
+	invalidArguemnt = 1,
+	noFeaturesEnabled = 2,
+	engineInitializationError = 3,
+	resourceAllocationError = 4
+};
+
+typedef int PipelineError;
+
+class IReplacementController : public IRelease
+{
+public:
+	virtual void setBackgroundImage(const IFrame* image) = 0;
+	virtual void clearBackgroundImage() = 0;
+};
+
+class IPipeline : public IRelease
+{
+public:
+	virtual PipelineError enableBlurBackground(float blurPower) = 0;
+	virtual void disableBackgroundBlur() = 0;
+	virtual bool getBlurBackgroundState(float* blurPower) const = 0;
+
+	virtual PipelineError enableReplaceBackground(IReplacementController** controller) = 0;
+	virtual void disableReplaceBackground() = 0;
+	virtual bool getReplaceBackgroundState() const = 0;
+	
+	virtual IFrame* process(const IFrame* input, PipelineError* error) = 0;
+};
+
+}
+
+#endif
